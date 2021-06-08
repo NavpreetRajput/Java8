6c3bbd9e06ea7a629968ea5ad1511d2c701676cc
Navpreet Rajput <84348431+NavpreetRajput@users.noreply.github.com>
2021-06-08 11:36:00 -0400

    Code Formatting and Commenting

--- src/Java8/ForEach.java
+++ src/Java8/ForEach.java
@@ -7,17 +7,28 @@ import java.util.List;
 
 public class ForEach {
 
-    public static void main(String[] args){
-        
+    public static void main(String[] args) {
+
         System.out.println("Names are: ");
+        // Initializing and declaring the array
         List<String> names = Arrays.asList("Ron", "Rick", "Pritam", "Jay", "Lucy", "Aamir", "Jon");
+
+        // Sorting the Array Alphabetically.
         Collections.sort(names);
+
+        // Printing the Array with the help of method Reference.
         names.forEach(System.out::println);
         System.out.println("\n");
-        
+
         System.out.println("Numbers are: ");
-        List<Integer> num = Arrays.asList(6, 7, 1, 5, 3, 9,0,4,8,2);
+
+        // Initializing and declaring the array
+        List<Integer> num = Arrays.asList(6, 7, 1, 5, 3, 9, 0, 4, 8, 2);
+
+        // Sorting the array numerically.
         num.sort(Comparator.comparing(Integer::valueOf));
+
+        // Printing the array with the help of Method Reference.
         num.forEach(System.out::println);
     }
 }
