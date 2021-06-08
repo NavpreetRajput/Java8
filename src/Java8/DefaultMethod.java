8143ec2f1c05846fa2505b1dd309505b558c20e0
Navpreet Rajput <84348431+NavpreetRajput@users.noreply.github.com>
2021-06-08 11:29:08 -0400

    Code Cleaning

--- src/Java8/DefaultMethod.java
+++ src/Java8/DefaultMethod.java
@@ -1,35 +1,37 @@
 package Java8;
 
-interface NewInterface{
-    /* This is a default method. There is no need
-     * to implement this method in the implementation 
-     * classes  
+interface NewInterface {
+    /*
+     * This is a default method. There is no need to implement this method in the
+     * implementation classes
      */
-    default void newMethod(){  
-        System.out.println("This is the default method");  
-    }  
-    
-    void existingMethod(String str);  
+    default void newMethod() {
+        System.out.println("This is the default method");
+    }
 
-    static void anotherNewMethod(){
-    	System.out.println("This is the static method");
+    void existingMethod(String str);
+
+    static void anotherNewMethod() {
+        System.out.println("This is the static method");
     }
 }
+
 public class DefaultMethod implements NewInterface {
-    public void method(){
-System.out.println("Hey, this is Navpreet Kaur.");
+    public void method() {
+        System.out.println("Hey, this is Navpreet Kaur.");
     }
 
-    public void existingMethod(String str){           
-        System.out.println("String is: "+str);  
-    }  
-    public static void main(String[] args) {  
+    public void existingMethod(String str) {
+        System.out.println("String is: " + str);
+    }
+
+    public static void main(String[] args) {
         DefaultMethod obj = new DefaultMethod();
-        
-        //calling the default method of interface
-        obj.newMethod();     
-        //calling the abstract method of interface
-        obj.existingMethod("JAVA 8 is interesting."); 
+
+        // calling the default method of interface
+        obj.newMethod();
+        // calling the abstract method of interface
+        obj.existingMethod("JAVA 8 is interesting.");
         NewInterface.anotherNewMethod();
     }
 }
