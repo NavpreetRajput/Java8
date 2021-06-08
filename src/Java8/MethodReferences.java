8008ca5127db61afbe8fffc3164a817064d2e35a
Navpreet Rajput <84348431+NavpreetRajput@users.noreply.github.com>
2021-06-08 11:40:50 -0400

    Code Formatting

--- src/Java8/MethodReferences.java
+++ src/Java8/MethodReferences.java
@@ -6,23 +6,23 @@ import java.util.function.BiFunction;
 
 public class MethodReferences {
 
-    
-    public MethodReferences(String say){
-    System.out.println(say);
-    }
-   static  Scanner sc = new Scanner(System.in);
-
-   /**
-    * Declaring an interface having display method
-    */
-    interface interfaces{  
-    void display(String say);  
+    public MethodReferences(String say) {
+        System.out.println(say);
     }
 
-     /**
+    static Scanner sc = new Scanner(System.in);
+
+    /**
+     * Declaring an interface having display method
+     */
+    interface interfaces {
+        void display(String say);
+    }
+
+    /**
      * declaring an interface having show method
      */
-    interface MyInterface{
+    interface MyInterface {
         void show();
     }
 
@@ -32,51 +32,51 @@ public class MethodReferences {
      * @param y y is the second number
      * @return returns the addition of two numbers
      */
-    public static int addition(int x,int y){
-        
-        return x+y;
+    public static int addition(int x, int y) {
+
+        return x + y;
     }
-    
-    public  void myMethod(){
+
+    public void myMethod() {
         System.out.println("Hey, I am Navpreet Kaur.");
-        
+
     }
 
-    public static void main (String[] args){
+    public static void main(String[] args) {
 
-        
-        
         /**
          * Method reference to a static Method of a class.
          */
         System.out.println("Enter any number of your choice :");
-        Integer add1=sc.nextInt();
+        Integer add1 = sc.nextInt();
         System.out.println("Enter another number of your choice :");
-        Integer add2=sc.nextInt();
-        BiFunction<Integer,Integer,Integer> add=MethodReferences::addition;
-        System.out.println("Addition of two numbers that you entered is: "+add.apply(add1,add2));
+        Integer add2 = sc.nextInt();
+        BiFunction<Integer, Integer, Integer> add = MethodReferences::addition;
+        System.out.println("Addition of two numbers that you entered is: " + add.apply(add1, add2));
         sc.close();
         /**
          * Method reference to an Intstance Method.
          */
-        MethodReferences mref=new MethodReferences("Hi");
-        MyInterface obj=mref::myMethod;
+        MethodReferences mref = new MethodReferences("Hi");
+        MyInterface obj = mref::myMethod;
         obj.show();
-     
+
         /**
-         * Method Reference to an instance method of an arbitrary object of a particular type
+         * Method Reference to an instance method of an arbitrary object of a particular
+         * type
          */
-        String[] array={"Navpreet Kaur", "Amandeep Kaur", "Amanjot Kaur", "Aaravbir Singh", "Hasrat", "Komalpreet Kaur","Agamjot Singh"};
+        String[] array = { "Navpreet Kaur", "Amandeep Kaur", "Amanjot Kaur", "Aaravbir Singh", "Hasrat",
+                "Komalpreet Kaur", "Agamjot Singh" };
         Arrays.sort(array, String::compareToIgnoreCase);
-        for(String arr: array ){
+        for (String arr : array) {
             System.out.println(arr);
         }
 
         /**
-         * Method reference to a constructor 
+         * Method reference to a constructor
          */
-        interfaces inter= MethodReferences::new;
+        interfaces inter = MethodReferences::new;
         inter.display("This program is finished. Thanks for joining.");
-        
+
     }
 }
